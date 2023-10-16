2a0e06c545ca8f7522b34113dac26a4acd033c92
andi <andiks2018@gmail.com>
2023-10-17 06:13:57 +0700

    tampilan auth page siakad selesai menit 30:01

--- lib/pages/auth/splash_page.dart
+++ lib/pages/auth/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../common/constants/images.dart';
+import 'auth_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -10,9 +11,28 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+
+
+    // delay 3 detik untuk masuk ke authpage
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const AuthPage(),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      //layar splash screen di sini seperti body html
       body: Center(
         child: Image.asset(
           Images.logo,
